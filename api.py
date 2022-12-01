c080440f86996d394454b0522a2895b860d4d067
Lokda-spec <gouarnec@gmail.com>
2022-12-01 12:21:29 +0100

    implemented flask

--- api.py
+++ api.py
@@ -24,7 +24,7 @@ class Locations(Resource):
     rows = cursor.execute("SELECT * FROM Frequentation").fetchall()
     pass
     
-api.add_resource(Users, '/users')  # '/users' is our entry point for Users
+api.add_resource(Users, '/db')  # '/users' is our entry point for Users
 api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
 
 if __name__ == '__main__':
