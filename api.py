9e94d1eaa2528edf8b984d74fd1944526786e09c
Clg <cg@jmg-conseil.eu>
2022-12-01 11:49:06 +0100

    corrected typo on sql query

--- api.py
+++ api.py
@@ -5,5 +5,5 @@ connection = sqlite3.connect("DataAnalyzer.db")
 
 cursor = connection.cursor()
 
-rows = cursor.execute("SELECT * FROM fish").fetchall()
+rows = cursor.execute("SELECT * FROM Frequentation").fetchall()
 print(rows)
