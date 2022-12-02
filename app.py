c3224f9adb5683dcf57e9825e48de48b01e85e8d
Clg <cg@jmg-conseil.eu>
2022-12-02 11:24:00 +0100

    serialized data

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import sqlite3
 from flask import Flask, render_template, redirect, request, json, jsonify, g, url_for
 from flask_restful import Resource, Api, reqparse
+import json
+import logging
+from datetime import date
+
+#logging.basicConfig(filename='./logs/logs_'+d1+'.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s : %(message)s')
 
 app = Flask(__name__)
+
+api = Api(app=app, version="1.0", doc="/db/api", title="Data analyzer DB service", description="Sqlite3 / Python-Flask", default="API", default_label='', validate=True)
 
 def get_db_connection():
     connection = sqlite3.connect('./db/DataAnalyzer.db')
@@ -21,7 +28,8 @@ def db():
 def jsonData():
     connection = get_db_connection()
     rows = connection.execute("SELECT * FROM Referentiel").fetchall()
-    return jsonify(rows)
+    jsonResult = json.dumps(rows, indent=4, sort_keys=True, default=str)
+    return jsonify(jsonResult)
 
 # debug
 if __name__ == "__main__":
