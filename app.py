a07b7acf36b064419c3b7840506ac3d88a163ccc
Clg <cg@jmg-conseil.eu>
2022-12-02 14:38:43 +0100

    added swaggerUI

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask import Flask, render_template, redirect, request, json, jsonify, g, url_for
 from flask_restful import Resource, Api, reqparse
 import json
@@ -11,7 +12,18 @@ logging.basicConfig(filename='./data/logs_'+d1+'.log', level=logging.INFO, forma
 
 app = Flask(__name__)
 
-#api = Api(app=app, version="1.0", doc="/db/api", title="Data analyzer DB service", description="Sqlite3 / Python-Flask", default="API", default_label='', validate=True)
+### swagger specific ###
+SWAGGER_URL = '/db/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Data Analyzer"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 def get_db_connection():
     connection = sqlite3.connect('data/DataAnalyzer.db')
